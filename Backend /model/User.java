5948f688e3aaa9d4d3f845e4cfc28adc6ed9b886
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-21 23:42:22 -0400

    Create User.java

---
+++ Backend /model/User.java
@@ -0,0 +1,22 @@
+package com.library.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    // Getters and Setters
+}

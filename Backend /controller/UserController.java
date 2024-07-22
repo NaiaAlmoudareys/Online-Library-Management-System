f83d2094e518955f43efe198129045d98de7459a
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-21 23:44:54 -0400

    Create UserController.java

---
+++ Backend /controller/UserController.java
@@ -0,0 +1,23 @@
+package com.library.controller;
+
+import com.library.model.User;
+import com.library.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @GetMapping("/{username}")
+    public User findByUsername(@PathVariable String username) {
+        return userService.findByUsername(username);
+    }
+}

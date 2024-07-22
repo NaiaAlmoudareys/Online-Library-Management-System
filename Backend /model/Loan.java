c41012f92b3a5dde30ba5354a3bf2ef9b96ccc14
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-21 23:42:58 -0400

    Create Loan.java

---
+++ Backend /model/Loan.java
@@ -0,0 +1,27 @@
+package com.library.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "loans")
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private Date loanDate;
+
+    private Date returnDate;
+
+    // Getters and Setters
+}

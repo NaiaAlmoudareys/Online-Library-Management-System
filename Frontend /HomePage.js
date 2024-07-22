39a1f74f052001f7117a9329b4009ab283660f07
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-21 23:46:07 -0400

    Create HomePage.js

---
+++ Frontend /HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Online Library Management System</h1>
+            <p>Manage your library with ease.</p>
+        </div>
+    );
+}
+
+export default HomePage;
